30f1e8b01743aa3a0cf5e9006d8f75d6dcacdc1c
dlthgud <sohyeong@konkuk.ac.kr>
2019-05-16 12:10:55 +0900

    RadioButton

--- app/src/main/java/com/example/enejwl/MainActivity.java
+++ app/src/main/java/com/example/enejwl/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.enejwl;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.example.enejwl.dlthgud.BackKeyClickHandler;
 import com.example.enejwl.dlthgud.GameActivity;
 import com.example.enejwl.dlthgud.Level;
+import com.example.enejwl.dlthgud.Mole;
 
 public class MainActivity extends AppCompatActivity {
-    public static Level level[] = new Level[3];
+    public static Activity mainActivity;
+    private BackKeyClickHandler backKeyClickHandler;
+
+    final static int MAX_LEVEL = 2;
+
+    public static Level level[] = new Level[MAX_LEVEL + 1];
+    public static Mole mole[] = new Mole[1];
 
     int curLevel;
     public static int lastLevel = 1;
-    final int MAX_LEVEL = 2;
 
     RadioGroup radioGroup;
     RadioGroup end_mode;
@@ -28,7 +38,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainActivity = MainActivity.this;
+        backKeyClickHandler = new BackKeyClickHandler(this);
+
         init();
+    }
+
+    @Override
+    public void onBackPressed() {
+        backKeyClickHandler.onBackPressed();
     }
 
     private void init() {
@@ -47,29 +65,39 @@ public class MainActivity extends AppCompatActivity {
             } else if(bool == 1) {
                 // TODO 성공 화면
                 curLevel++; // 레벨 업
-                lastLevel = curLevel;
+                if(curLevel > MAX_LEVEL) {
+                    curLevel = MAX_LEVEL;
+                }
+                if(lastLevel < curLevel) {
+                    lastLevel = curLevel;
+                }
             }
         }
 
         // TODO 두더지 객체 생성
+        mole[0] = new Mole("두더지", 3, 3, 2, 5, R.drawable.enejwl);
         // TODO 아이템 객체 생성
 
         // 레벨 객체 생성
-        int[] map_3 = {1,0,0,0,0,0,0,0,0};
+        int[] map_3 = {0,1,0,1,1,1,0,1,0};
 //        int[] map_4 = {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0};
-        int[] map_5 = {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0,0,1,0};
+        int[] map_5 = new int[25];
+        for (int i=0; i<25; i++) {
+            map_5[i] = 1;
+        }
         level[1] = new Level(map_3, 3, 3, 40, 1, 0);
         level[2] = new Level(map_5, 5, 5, 50, 1, 0);
 
         radioGroup = (RadioGroup) findViewById(R.id.level);
 
         for(int i=1; i<=lastLevel; i++) {
-            if(i>MAX_LEVEL){
+            if(lastLevel > MAX_LEVEL) {
                 break;
             }
             RadioButton radioButton = new RadioButton(this);
             radioButton.setText("Level " + i);
-            radioButton.setId(getResources().getIdentifier("level_" + i, "id", package_name));
+            radioButton.setId(i);
+            // getResources().getIdentifier("level_" + i, "id", package_name)
             RadioGroup.LayoutParams layoutParams = new RadioGroup.LayoutParams(
                     RadioGroup.LayoutParams.WRAP_CONTENT,
                     RadioGroup.LayoutParams.WRAP_CONTENT);
@@ -83,23 +111,33 @@ public class MainActivity extends AppCompatActivity {
             curLevel = 1;
         }
 
+        Toast.makeText(this, "레벨: " + curLevel, Toast.LENGTH_SHORT).show();
+
         if(curLevel == 0) {
             radioGroup.check(getResources().getIdentifier("user_mode", "id", package_name));
         } else {
-            radioGroup.check(getResources().getIdentifier("level_" + curLevel, "id", package_name));
+            radioGroup.check(curLevel);
+            // getResources().getIdentifier("level_" + curLevel, "id", package_name)
         }
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 int id = radioGroup.getCheckedRadioButtonId();
+                Log.d("level", "onCheckedChanged: " + id);
                 RadioButton radioButton = (RadioButton) findViewById(id);
                 String level_text = radioButton.getText().toString();
+                Log.d("level", "onCheckedChanged: " + level_text);
                 String[] array = level_text.split(" ");
-                curLevel = Integer.parseInt(array[1]);
+                Log.d("level", "onCheckedChanged: " + array[1]);
+                if(array[1].equals("모드")) {
+                    curLevel = 0;
+                } else {
+                    curLevel = Integer.parseInt(array[1]);
+                }
+//                Toast.makeText(getApplicationContext(), "레벨: " + curLevel, Toast.LENGTH_SHORT).show();
                 if(curLevel == 0) {
                     end_mode.setVisibility(View.INVISIBLE);
-                    // TODO 레벨 조건 입력
                 } else {
                     end_mode.setVisibility(View.VISIBLE);
                 }
@@ -109,17 +147,6 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {   // 시작 버튼 클릭 시
             @Override
             public void onClick(View v) {
-//                int level_id = radioGroup.getCheckedRadioButtonId();
-//                RadioButton radioButton = (RadioButton) findViewById(level_id);
-//                String level_text = radioButton.getText().toString();
-//                String[] array = level_text.split(" ");
-//                curLevel = Integer.parseInt(array[1]);
-//                if(curLevel == 0) {
-//                    end_mode.setVisibility(View.INVISIBLE);
-//                    // TODO 레벨 조건 입력
-//                } else {
-//                    end_mode.setVisibility(View.VISIBLE);
-//                }
                 if(curLevel != 0) {
                     int end_id = end_mode.getCheckedRadioButtonId();
                     RadioButton radioButton1 = (RadioButton) findViewById(end_id);
@@ -136,6 +163,8 @@ public class MainActivity extends AppCompatActivity {
                             level[2].setCondition(3);
                             break;
                     }
+                } else {
+                    // TODO 레벨 조건 입력
                 }
                 // intent에서 "curLevel" curLevel 값 보내기
                 Intent intent = new Intent(getApplicationContext(), GameActivity.class);
---
+++ app/src/main/java/com/example/enejwl/dlthgud/Mole.java
@@ -0,0 +1,67 @@
+package com.example.enejwl.dlthgud;
+
+public class Mole { // 두더지의 정보를 저장하는 클래스
+    String name;    // 두더지 이름
+    int touch;  // 두더지를 없애기 위한 터치 수
+    int score;  // 점수
+    int upMax;  // 두더지가 올라와 있는 최대 시간
+    int upMin;  // 두더지가 올라와 있는 최소 시간
+    int image;  // 화면에 나타날 이미지
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTouch(int touch) {
+        this.touch = touch;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setUpMax(int upMax) {
+        this.upMax = upMax;
+    }
+
+    public void setUpMin(int upMin) {
+        this.upMin = upMin;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTouch() {
+        return touch;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getUpMax() {
+        return upMax;
+    }
+
+    public int getUpMin() {
+        return upMin;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public Mole(String name, int touch, int score, int upMax, int upMin, int image) {
+        this.name = name;
+        this.touch = touch;
+        this.score = score;
+        this.upMax = upMax;
+        this.upMin = upMin;
+        this.image = image;
+    }
+}
